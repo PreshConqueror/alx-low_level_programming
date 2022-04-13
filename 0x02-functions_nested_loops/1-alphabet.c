5da26f7577425a19c59acaec079b0ae0ebee3a2b
PreshConqueror <pconqueror07@gmail.com>
2022-04-12 18:49:19 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*main - Entry point
+*
+*print_alphabet - print alphabet lowercase
+*
+*Return: Always 0
+*/
+void print_alphabet(void)
+{
+char d;
+for (d = 'a'; d <= 'z'; d++)
+_putchar(d);
+_putchar('\n');
+}
