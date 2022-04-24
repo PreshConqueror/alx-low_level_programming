95f29556b8ccc80dd4d8d4aba56844c4fc476976
Precious Conqueror <70549991+PreshConqueror@users.noreply.github.com>
2022-04-24 20:41:15 +0200

    new

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,13 +8,14 @@
 
 int main(void)
 {
-	int i;
-	char p[] = "_putchar";
+	char text[9] = "_putchar";
+	int i = 0;
 
-	for (i = 0; i < 9; i++)
-		{
-		_putchar(p[i]);
-		}
-		_putchar('\n');
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
 	return (0);
 }
