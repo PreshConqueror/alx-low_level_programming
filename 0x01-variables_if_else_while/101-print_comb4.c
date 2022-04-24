18deb54e8145b874909513479fe6d3d3e7d86d91
Precious Conqueror <70549991+PreshConqueror@users.noreply.github.com>
2022-04-24 20:14:14 +0200

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,12 +10,12 @@
  */
 int main(void)
 {
-int i,j,k;
+int i, j, k;
 for (i = 48; i < 58; i++)
 {
 for (j = 49; j < 58; j++)
 {
-for (k =50; k < 58; k++)
+for (k = 50; k < 58; k++)
 {
 if (k > j && j > 1)
 {
@@ -32,5 +32,6 @@ putchar(' ');
 }
 }
 putchar('\n');
+
 return (0); 
 }
