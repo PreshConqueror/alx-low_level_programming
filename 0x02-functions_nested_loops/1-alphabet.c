fd8d4f4c54ed0888eb410c6dd53217000d1bc1b6
PreshConqueror <pconqueror07@gmail.com>
2023-09-12 06:35:36 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*print_alphabet - print alphabet lowercase
+*
+*Return: Always 0
+*/
+void print_alphabet(void)
+{
+int i;
+
+	for (i = 97; i < 123; i++)
+		{
+		_putchar(i);
+		}
+		_putchar('\n');
+}
